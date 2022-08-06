ac18000e793e2364631f48b3636669198ed6abce
Ankit <kumarankit1041999@gmail.com>
2022-08-06 10:18:28 +0530

    Basic done

--- constants/Errors.mjs
+++ constants/Errors.mjs
@@ -1,31 +1,31 @@
 const Errors = {
   ORDER_NOT_FOUND_BY_ID: {
-    status: 401,
+    status: 404,
     message: 'Order Not Found By Id',
     code: 'ERROR_01'
   },
   BAD_REQUEST: {
-    status: 402,
+    status: 400,
     message: 'Bad Request',
     code: 'ERROR_02'
   },
   TRACKER_NOT_FOUND_BY_UNIQUE_IDF: {
-    status: 401,
+    status: 404,
     message: 'Tracker Not Found',
     code: 'ERROR_03'
   },
   INSUFFICIENT_CAPACITY: {
-    status: 401,
+    status: 200,
     message: 'Insufficient Capacity',
     code: 'ERROR_04'
   },
   TRACKER_NOT_FOUND_BY_ID: {
-    status: 401,
+    status: 404,
     message: 'Tracker Not Found By Id',
     code: 'ERROR_05'
   },
   TRACKER_ALREADY_EXISTS: {
-    status: 401,
+    status: 404,
     message: 'Tracker Already Exists',
     code: 'ERROR_05'
   }
